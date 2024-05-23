d5222c7598910292fc9c1024229ee8348ea06489
AyuTUL <ayushtuladhar007@gmail.com>
2024-05-23 14:00:58 +0545

    sdfg

--- 3rdla.c
+++ 3rdla.c
@@ -1,5 +1,3 @@
-//Student Record Management System
-
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
@@ -15,7 +13,7 @@ struct subject
 struct marks
 {
 	float total,percent;
-	char sch[5],gr[4];
+	char gr[5];
 };
 
 struct student
@@ -28,26 +26,26 @@ struct student
 
 typedef struct student rec;
 
+// Function prototypes
+FILE* open(char [],char []);
+rec add(FILE *);
+void ascn(rec [],int);
+void ascp(rec [],int);
 char confirm(char []);
 void create();
-rec add(FILE *);
-void view();
-void update();
 void del();
-void save(char [],rec);
-FILE* open(char [],char []);
+void descn(rec [],int);
+void descp(rec [],int);
+void prinTab(rec [],int);
 void remo(char []);
 void rena(char [],char []);
-void viewAll(FILE *,rec);
-void viewFac(FILE *,char [],rec);
+void save(char [],rec);
 void search();
-void prin1(rec);
-void prin2(rec);
-void prin3(rec,int);
-void prin4(rec,int);
-void ascn(rec [],int);
-void descn(rec [],int);
-void prinTab(rec *,int);
+void swap(rec *,rec *);
+void update();
+void view();
+void printCheck(rec);
+void viewFac(FILE *,char [],rec);
 
 void main()
 {
@@ -87,15 +85,78 @@ void main()
 	}while(ch=='y' || ch=='Y');
 }
 
-
-void prin1(rec s)
+void create()
 {
-	printf("%-17s: %s\n%-17s: %s\n%-17s: %s\n%-17s: %s\n%-17s: %d\n%-17s: %s\n%-17s: %.2f %%\n\n","Name",s.name,"Address",s.addr,"Registration No.",s.regNum,"Faculty",s.faculty,"Semester",s.sem,"Grade",s.m.gr,"Percentage",s.m.percent);
+	FILE *fp;
+	rec s;
+	fp=open("record.txt","ab+");
+	printf("Enter the following student details :\n");
+	s=add(fp);
+	save("record.txt",s);
+	printf("File Successfully saved.");
 }
 
-void prin2(rec s)
+FILE* open(char fn[],char mode[])
 {
-	printf("%-17s: %s\n%-17s: %s\n%-17s: %s\n%-17s: %s\n%-17s: %d\n%-17s: %s\n%-17s: N/A\n\n","Name",s.name,"Address",s.addr,"Registration No.",s.regNum,"Faculty",s.faculty,"Semester",s.sem,"Grade",s.m.gr,"Percentage");
+	FILE *fp;
+	fp=fopen(fn,mode);
+	if(fp==NULL)
+	{
+		printf("Error opening file.");
+		exit(1);
+	}
+	return(fp);
+}
+
+rec add(FILE *fp)
+{
+	rec s,temp;
+	int i;
+	printf("Registration No. : ");
+	fscanf(stdin,"%s",s.regNum);
+	while(fread(&temp,sizeof(rec),1,fp))
+	{
+		while(!strcmp(temp.regNum,s.regNum))
+		{
+			printf("Student record with registration no. %s already exists.\nEnter registration no. again : ",s.regNum);
+			fflush(stdin);
+			fscanf(stdin,"%s",s.regNum);
+			rewind(fp);
+		}
+	}
+	fclose(fp);
+	printf("Name : ");
+	fflush(stdin);
+	fgets(s.name,sizeof(s.name),stdin);
+	strtok(s.name,"\n");
+	printf("Faculty : ");
+	fgets(s.faculty,sizeof(s.faculty),stdin);
+	strtok(s.faculty,"\n");
+	printf("Semester : ");
+	scanf("%d",&s.sem);
+	printf("Address : ");
+	fflush(stdin);
+	fgets(s.addr,sizeof(s.addr),stdin);
+	strtok(s.addr,"\n");
+	strcpy(s.m.gr,"PASS");
+	s.m.total=0;
+	for(i=0;i<5;i++)
+	{
+		printf("Name of Subject %d : ",i+1);
+		fflush(stdin);
+		fgets(s.sub[i].subName,sizeof(s.sub[i].subName),stdin);
+		strtok(s.sub[i].subName,"\n");
+		printf("Marks : ");
+		scanf("%f",&s.sub[i].marks);
+		if(s.sub[i].marks<40)
+		{
+			strcpy(s.m.gr,"FAIL");
+			continue;
+		}
+		s.m.total+=s.sub[i].marks;
+	}
+	s.m.percent=s.m.total/5;
+	return(s);
 }
 
 void remo(char fn[])
@@ -122,81 +183,8 @@ void rena(char old[],char newn[])
 
 char confirm(char s[])
 {
-	char ch;
 	printf("\n%s",s);
 	return(getche());
-}
-
-FILE* open(char fn[],char mode[])
-{
-	FILE *fp;
-	fp=fopen(fn,mode);
-	if(fp==NULL)
-	{
-		printf("Error opening file.");
-		exit(1);
-	}
-	return(fp);
-}
-
-void create()
-{
-	FILE *fp;
-	rec s;
-	fp=open("record.txt","ab+");
-	printf("Enter the following student details :\n");
-	s=add(fp);
-	save("record.txt",s);
-	printf("File Successfully saved.");
-}
-
-rec add(FILE *fp)
-{
-	rec s,temp;
-	int i;
-	printf("Registration No. : ");
-	fscanf(stdin,"%s",s.regNum);
-	while(fread(&temp,sizeof(rec),1,fp))
-	{
-		while(!strcmp(temp.regNum,s.regNum))
-		{
-			printf("Student record with registration no. %s already exists.\nEnter registration no. again : ",s.regNum);
-			fflush(stdin);
-			fscanf(stdin,"%s",s.regNum);
-			rewind(fp);
-		}
-	}
-	fclose(fp);
-	printf("Name : ");
-	fflush(stdin);
-	fgets(s.name,sizeof(s.name),stdin);
-	printf("Faculty : ");
-	fflush(stdin);
-	fgets(s.faculty,sizeof(s.faculty),stdin);
-	printf("Semester : ");
-	scanf("%d",&s.sem);
-	printf("Address : ");
-	fflush(stdin);
-	fgets(s.addr,sizeof(s.addr),stdin);
-	strcpy(s.m.gr,"PASS");
-	s.m.total=0;
-	for(i=0;i<5;i++)
-	{
-		printf("Name of Subject %d : ",i+1);
-		fflush(stdin);
-		fgets(s.sub[i].subName,sizeof(s.sub[i].subName),stdin);
-		printf("Marks : ");
-		scanf("%f",&s.sub[i].marks);
-		if(s.sub[i].marks<40)
-		{
-			strcpy(s.m.gr,"FAIL");
-			s.m.percent=0;
-			break;
-		}
-		s.m.total+=s.sub[i].marks;
-	}
-	s.m.percent=s.m.total/5;
-	return(s);
 }
 
 void save(char fn[],rec s)
@@ -204,26 +192,25 @@ void save(char fn[],rec s)
 	FILE *fp;
 	int i;
 	fp=open(fn,"ab");
-	if(!fwrite(&s,sizeof(s),1,fp))
+	if(fwrite(&s,sizeof(s),1,fp)!=1)
 		printf("Error writing into file.");
 	fclose(fp);
 }
 
-void viewAll(FILE *fp,rec s)
+
+void printCheck(rec s)
 {
-	while(fread(&s,sizeof(rec),1,fp))
-	{
-		if(strcmp(s.m.gr,"PASS")==0)
-			prin1(s);
-		else
-			prin2(s);
-	}
+	if(strcmp(s.m.gr,"PASS")==0)
+		printf("%-17s: %s\n%-17s: %s\n%-17s: %s\n%-17s: %s\n%-17s: %d\n%-17s: %s\n%-17s: %.2f %%\n\n","Name",s.name,"Address",s.addr,"Registration No.",s.regNum,"Faculty",s.faculty,"Semester",s.sem,"Grade",s.m.gr,"Percentage",s.m.percent);
+	else
+		printf("%-17s: %s\n%-17s: %s\n%-17s: %s\n%-17s: %s\n%-17s: %d\n%-17s: %s\n%-17s: N/A\n\n","Name",s.name,"Address",s.addr,"Registration No.",s.regNum,"Faculty",s.faculty,"Semester",s.sem,"Grade",s.m.gr,"Percentage");
 }
 
 void viewFac(FILE *fp,char fac[],rec s)
 {
-	int c=0,ch,i;
+	int c=0,ch,i,j;
 	rec sfac[MAX];
+	strupr(fac);
 	while(fread(&s,sizeof(rec),1,fp))
 	{
 		strupr(s.faculty);
@@ -233,67 +220,60 @@ void viewFac(FILE *fp,char fac[],rec s)
 			c++;
 		}
 	}
-	printf("1. View all Student Record\n2. Ascending by Name\n3. Descending by Name\n4. Ascending by Percentage\n5. Descending by Percentage");
+	printf("\n1. View all Student Record\n2. Ascending by Name\n3. Descending by Name\n4. Ascending by Percentage\n5. Descending by Percentage");
 	ch=confirm("\nEnter your choice : ");
 	system("cls");
 	switch(ch)
 	{
 		case '1':
-			viewAll(fp,s);
+			for(i=0;i<c;i++)
+			{
+				printCheck(sfac[i]);
+			}
 			break;
 		case '2':
-			ascn(sfac,c);
-			prinTab(sfac,c);
-			break;
 		case '3':
-			descn(sfac,c);
-			prinTab(sfac,c);
-			break;
 		case '4':
-			//();
-			break;
 		case '5':
-			//search();
+			for(i=0;i<c;i++)
+				{
+					for(j=0;j<c-i-1;j++)
+					{
+						switch(ch)
+						{
+							case '2':
+								if(strcmp((sfac+j)->name,(sfac+j+1)->name)>0)
+									swap(&sfac[j],&sfac[j+1]);
+								break;
+							case '3':
+								if(strcmp((sfac+j)->name,(sfac+j+1)->name)<0)
+									swap(&sfac[j],&sfac[j+1]);
+								break;
+							case '4':
+	                            if((sfac+j)->m.percent>(sfac+j+1)->m.percent)
+	                                swap(&sfac[j],&sfac[j+1]);
+	                            break;
+	                        case '5':
+	                            if((sfac+j)->m.percent<(sfac+j+1)->m.percent)
+	                                swap(&sfac[j],&sfac[j+1]);
+	                            break;
+						}
+						
+					}
+				}
+				prinTab(sfac,c);
 			break;
 		default:
 			printf("Invalid choice. Please choose from 1 - 5.");
 	}
 }
 
-void ascn(rec *s,int n)
+void swap(rec *a,rec *b)
 {
-	int i,j;
 	rec temp;
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<n-i-1;j++)
-		{
-			if(strcmp((s+j)->name,(s+j+1)->name)>0)
-			{
-				temp=*(s+j);
-                *(s+j)=*(s+j+1);
-                *(s+j+1)=temp;
-			}
-		}
-	}
-}
-
-void descn(rec *s,int n)
-{
-	int i,j;
-	rec temp;
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<n-i-1;j++)
-		{
-			if(strcmp((s+j)->name,(s+j+1)->name)<0)
-			{
-				temp=*(s+j);
-                *(s+j)=*(s+j+1);
-                *(s+j+1)=temp;
-			}
-		}
-	}
+	temp=*a;
+    *a=*b;
+    *b=*a;
 }
 
 void prinTab(rec s[],int n)
@@ -303,27 +283,18 @@ void prinTab(rec s[],int n)
 	for(i=0;i<n;i++)
 	{
 		if(strcmp(s[i].m.gr,"FAIL")==0)
-			prin4(*(s+i),i);
+			printf("\n| %-20s | %-15s | %-16s | %-7s | %8d | %-5s | %10s |",s[i].name,s[i].addr,s[i].regNum,s[i].faculty,s[i].sem,s[i].m.gr,"N/A");
 		else
-			prin3(*(s+i),i);
+			printf("\n| %-20s | %-15s | %-16s | %-7s | %8d | %-5s | %8.2f %% |",s[i].name,s[i].addr,s[i].regNum,s[i].faculty,s[i].sem,s[i].m.gr,s[i].m.percent);
 	}
-}
-
-void prin3(rec s,int i)
-{
-	printf("\n| %-20s | %-15s | %-16s | %-7s | %8d | %-5s | %8.2f %% |",s.name,s.addr,s.regNum,s.faculty,s.sem,s.m.gr,s.m.percent);
-}
-
-void prin4(rec s,int i)
-{
-	printf("\n| %-20s | %-15s | %-16s | %-7s | %ds | %-5s | %8s |",s.name,s.addr,s.regNum,s.faculty,s.sem,s.m.gr,"N/A");
 }
 
 void view()
 {
 	FILE *fp;
 	rec s;
-	int c,ch;
+	int c;
+	char fac[4];
 	fp=open("record.txt","rb");
 	printf("1. View all Student Record\n2. View Student Record according to Faculty");
 	c=confirm("\nEnter your choice : ");
@@ -331,29 +302,21 @@ void view()
 	{
 		case '1':
 			system("cls");
-			viewAll(fp,s);
-			break;
-		case '2':
-			printf("\n\n1. CSIT\n2. BIM\n3. BHM");
-			ch=confirm("\nEnter your choice : ");
-			system("cls");
-			switch(ch)
+			while(fread(&s,sizeof(rec),1,fp))
 			{
-				case '1':
-					viewFac(fp,"CSIT\n",s);
-					break;
-				case '2':
-					viewFac(fp,"BIM\n",s);
-					break;
-				case '3':
-					viewFac(fp,"BHM\n",s);
-					break;
-				default:
-					printf("Invalid choice. Please choose between 1-3.");
+				printCheck(s);
 			}
 			break;
+		case '2':
+			printf("\nEnter Faculty : ");
+            fflush(stdin);
+            fgets(fac,sizeof(fac),stdin);
+            strtok(fac,"\n");
+            strupr(fac);
+            viewFac(fp,fac,s);
+			break;
 		default:
-			printf("Invalid choice. Please choose between 1 & 2.");
+			printf("\nInvalid choice. Please choose between 1 & 2.");
 	}
 	fclose(fp);
 }
@@ -373,9 +336,9 @@ void update()
 	{
 		if(strcmp(reg,s1.regNum)==0)
 		{
+			flag=1;	
 			printf("Enter new details :\n");
-			s1=add(fp2);
-			flag=1;		
+			s1=add(fp2);	
 		}
 		save("temp.txt",s1);
 	}
@@ -419,7 +382,7 @@ void del()
 	}
 	else
 	{
-		printf("File successfully deleted.");
+		printf("Record successfully deleted.");
 	}
 	fclose(fp2);
 	fclose(fp1);
@@ -442,7 +405,7 @@ void search()
 		if(strcmp(reg,s.regNum)==0)
 		{
 			flag=1;
-			printf("%-17s: %s%-17s: %s%-17s: %s\n%-17s: %s%-17s: %d\n%-17s: %.2f %%\n\n","Name",s.name,"Address",s.addr,"Registration No.",s.regNum,"Faculty",s.faculty,"Semester",s.sem,"Percentage",s.m.percent);	
+			printCheck(s);
 		}
 	}
 	if(flag==0)
--- test.c
+++ test.c
@@ -1,76 +1,90 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h> // Include time.h for randomization
 
-#define MAX 100
-
+// Define the structure for student records
 struct subject {
-    char subName[MAX];
+    char subName[100];
     float marks;
 };
 
 struct marks {
     float total, percent;
-    const char *sch, *gr;
+    char gr[5];
 };
 
 struct student {
     int sem;
-    char name[MAX], addr[MAX], regNum[MAX], faculty[MAX];
+    char name[100], addr[100], regNum[100], faculty[100];
     struct subject sub[5];
-    struct marks m;
+    struct marks m; 
 };
 
-typedef struct student rec;
-
 int main() {
-    rec students[3];
     FILE *fp;
-int i;
-    // Initialize student data
-    students[0].sem = 1;
-strcpy(students[0].name, "Charlie");
-strcpy(students[0].addr, "Addr1");
-strcpy(students[0].regNum, "Reg1");
-strcpy(students[0].faculty, "CSIT");
+    struct student records[30]; // Sample data for 30 students
+    int i, j, k;
 
-strcpy(students[0].sub[0].subName, "Math");
-students[0].sub[0].marks = 12;
+    // Initialize random seed
+    srand(time(NULL));
 
-strcpy(students[0].sub[1].subName, "Physics");
-students[0].sub[1].marks = 12;
+    // Initialize sample student records
+    for (i = 0; i < 30; i++) {
+        sprintf(records[i].regNum, "REG%d", i + 1);
+        strcpy(records[i].name, "Student Name");
+        strcpy(records[i].addr, "Address");
+        
+        // Randomly assign faculty
+        int facultyIndex = rand() % 4;
+        switch (facultyIndex) {
+            case 0:
+                strcpy(records[i].faculty, "CSIT");
+                break;
+            case 1:
+                strcpy(records[i].faculty, "BHM");
+                break;
+            case 2:
+                strcpy(records[i].faculty, "BIM");
+                break;
+            case 3:
+                strcpy(records[i].faculty, "BBM");
+                break;
+            default:
+                strcpy(records[i].faculty, "CSIT"); // Default to CSIT
+        }
 
-strcpy(students[0].sub[2].subName, "IIT");
-students[0].sub[2].marks = 12;
+        records[i].sem = (i % 8) + 1; // Random semester between 1 and 8
 
-strcpy(students[0].sub[3].subName, "C");
-students[0].sub[3].marks = 12;
+        // Generate random subject names and marks
+        for (j = 0; j < 5; j++) {
+            sprintf(records[i].sub[j].subName, "Subject%d", j + 1);
+            records[i].sub[j].marks = (rand() % 71) + 30; // Random marks between 30 and 100
+        }
 
-strcpy(students[0].sub[4].subName, "DL");
-students[0].sub[4].marks = 12;
-
-students[0].m.total = 60.0;
-students[0].m.percent = (students[0].m.total / 60.0) * 100;
-
-
-    students[0].m.gr = "PASS";
-
+        strcpy(records[i].m.gr, "PASS"); // Default grade
+        records[i].m.total = 0;
+        for (k = 0; k < 5; k++) {
+            if (records[i].sub[k].marks < 40) {
+                strcpy(records[i].m.gr, "FAIL");
+                break;
+            }
+            records[i].m.total += records[i].sub[k].marks;
+        }
+        records[i].m.percent = records[i].m.total / 5.0;
+    }
 
     // Open file in binary write mode
     fp = fopen("record.txt", "wb");
-    if (!fp) {
-        printf("Error opening file.\n");
-        return 1;
-    }
 
-    // Write student data to file
-    for ( i = 0; i < 3; i++) {
-        fwrite(&students[i], sizeof(rec), 1, fp);
-    }
+    // Write data to the binary file
+    fwrite(records, sizeof(struct student), 30, fp);
 
-    // Close file
+    // Close the file
     fclose(fp);
 
-    printf("Student data written to students.dat successfully.\n");
+    printf("Data written to record.bin.\n");
 
     return 0;
 }
+
